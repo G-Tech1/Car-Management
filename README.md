ecc6fde8d98172413e7b5232d0cae59de075753b
Gilan Serrant <gilserrant5@gmail.com>
2022-06-17 20:08:40 -0400

    made technician model

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Team:
 
-* Person 1 - Which microservice?
-* Person 2 - Which microservice?
+- Gilan- Auto Service
+- Person 2 - Which microservice?
 
 ## Design
 
--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Technician(models.Model):
+    name = models.CharField(max_length=200)
+    employee_number = models.PositiveBigIntegerField()
+
