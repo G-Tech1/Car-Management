cec2b2791f441d2669f4b730c31833997ac3ceda
Brandon Nieves <Bnieves1223@hotmail.com>
2022-06-21 15:02:53 -0400

    finished the poller and the backend stuff

--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import SalesPerson
+from .models import SalesPerson,Customer,Record
 
 # Register your models here.
 
 @admin.register(SalesPerson)
 class SalesPersonAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
     pass
--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -5,10 +5,12 @@ import time
 import json
 import requests
 
+
 sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_project.settings")
 django.setup()
 
+from sales_rest.models import AutoVO
 # Import models from sales_rest, here.
 # from sales_rest.models import Something
 
@@ -16,11 +18,15 @@ def poll():
     while True:
         print('Sales poller polling for data')
         try:
-            # Write your polling logic, here
-            pass
+            response = requests.get("http://inventory-api:8000/api/automobiles/")
+            content = json.loads(response.content)
+            for cars in content['autos']:
+                AutoVO.objects.update_or_create(
+                    vin=cars['vin']
+                )
         except Exception as e:
             print(e, file=sys.stderr)
-        time.sleep(60)
+        time.sleep(5)
 
 
 if __name__ == "__main__":
