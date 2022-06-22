b55fd93ff30953b72d402ba3144338b4ff751ca5
Brandon Nieves <Bnieves1223@hotmail.com>
2022-06-21 20:26:52 -0400

    Customer form done

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CustomerForm from './CustomerForm';
 import MainPage from './MainPage';
 import Nav from './Nav';
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/Customer/" element={<CustomerForm />} />
         </Routes>
       </div>
     </BrowserRouter>
