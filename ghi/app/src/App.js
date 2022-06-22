f71a8e79937392dc7e5e1807eaf38000cdd9e546
Brandon Nieves <Bnieves1223@hotmail.com>
2022-06-21 21:36:54 -0400

    Finished new employee form

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CustomerForm from './CustomerForm';
+import SalesForm from './SalesForm';
 import MainPage from './MainPage';
 import Nav from './Nav';
 
@@ -11,6 +12,7 @@ function App(props) {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/Customer/" element={<CustomerForm />} />
+          <Route path="/sales/" element={<SalesForm />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ghi/app/src/SalesForm.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+class SalesForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            employeeNumber: '',
+        };
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleEmployeeNumberChange = this.handleEmployeeNumberChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+
+
+
+
+
+
+    async handleSubmit(event) {
+        event.preventDefault();
+        const data = { ...this.state };
+        data.employee_number = data.employeeNumber;
+        delete data.employeeNumber;
+
+        const customerURL = 'http://localhost:8090/api/sales/'
+        const fetchConfig = {
+            method: 'post',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+        const response = await fetch(customerURL, fetchConfig);
+        if (response.ok) {
+            const newEmployee = await response.json();
+            console.log(newEmployee)
+
+            const cleared = {
+                name: '',
+                employeeNumber: ''
+            }
+            this.setState(cleared)
+        }
+    }
+    handleNameChange(event) {
+        const value = event.target.value;
+        this.setState({ name: value });
+    }
+
+    handleEmployeeNumberChange(event) {
+        const value = event.target.value;
+        this.setState({ employeeNumber: value });
+    }
+
+
+    render() {
+        return (
+            <div className="row">
+                <div className="offset-3 col-6">
+                    <div className="shadow p-4 mt-4">
+                        <h1>Create a new employee</h1>
+                        <form onSubmit={this.handleSubmit} id="create-Sales-form">
+                            <div className="form-floating mb-3">
+                                <input value={this.state.name} onChange={this.handleNameChange} placeholder="Name" required type="text" name="name" id="name" className="form-control" />
+                                <label htmlFor="name">Name</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input value={this.state.employeeNumber} onChange={this.handleEmployeeNumberChange} placeholder="employeeNumber" required type="text" name="employeeNumber" id="employee_number" className="form-control" />
+                                <label htmlFor="phoneNumber">Employee Number</label>
+                            </div>
+                            <button className="btn btn-primary">Create</button>
+                        </form>
+                    </div>
+
+                </div>
+            </div >
+        );
+    }
+}
+
+export default SalesForm;
