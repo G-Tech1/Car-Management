091abd41ed98221ef6766029329c3850882b3350
Gilan Serrant <gilserrant5@gmail.com>
2022-06-21 20:38:28 -0400

    added technician form service form wip

---
+++ ghi/app/src/TechnicianForm.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+class TechnicianForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      employeeNumber: '',
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeEmployeeNumber = this.handleChangeEmployeeNumber.bind(this);
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    const data = {...this.state};
+    data.employee_number = data.employeeNumber
+    delete data.employeeNumber;
+
+
+    const technicianUrl = 'http://localhost:8080/api/technician/';
+    const fetchOptions = {
+      method: 'post',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const technicianResponse = await fetch(technicianUrl, fetchOptions);
+    if (technicianResponse.ok) {
+        const newTechnician = await technicianResponse.json();
+        console.log(newTechnician);
+        
+        const cleared = {
+            name:'',
+            employeeNumber:''
+        };
+        
+        this.setState(cleared);
+    }
+  }
+
+
+  handleChangeName(event) {
+    const value = event.target.value;
+    this.setState({ name: value });
+  }
+
+  handleChangeEmail(event) {
+    const value = event.target.value;
+    this.setState({ employeeNumber: value });
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="offset-3 col-6">
+          <div className="shadow p-4 mt-4">
+            <h1>Add A Technician</h1>
+            <form onSubmit={this.handleSubmit} id="create-location-form">
+              <div className="form-floating mb-3">
+                <input value={this.state.name} onChange={this.handleNameChange} placeholder="Name" required type="text" name="name" id="name" className="form-control" />
+                <label htmlFor="name">Name</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input value={this.state.roomCount} onChange={this.handleEmployeeNumberChange} placeholder="Employee Number" required type="number" name="employee_number" id="employee_number" className="form-control" />
+                <label htmlFor="employee_number">Room count</label>
+              </div>
+              <button className="btn btn-primary">Add</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+}
+
+export default TechnicianForm;
