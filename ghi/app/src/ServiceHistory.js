1167328af2af741ad2a952b92f29eb51429d311a
Gilan Serrant <gilserrant5@gmail.com>
2022-06-23 23:14:35 -0400

    added all history for service

--- ghi/app/src/ServiceHistory.js
+++ ghi/app/src/ServiceHistory.js
@@ -8,7 +8,7 @@ import React from 'react';
                 services: []
             }
 
-            this.handleOnSubmit = this.handleOnSubmit.bind(this);
+            // this.handleOnSubmit = this.handleOnSubmit.bind(this);
             this.handleVinChange = this.handleVinChange.bind(this);
         };
         
@@ -29,33 +29,33 @@ import React from 'react';
     }
 
 
-    async handleOnSubmit(event) {
-        event.preventDefault()
-        const newState = [...this.state ]
-        delete newState.services 
-        const servicesUrl = `http://localhost:8080/api/service/`;
-        const fetchConfig = {
-            method: "get",
-            headers: { 'Content-Type': 'application/json' },
-        }
-        const response = await fetch(servicesUrl, fetchConfig)
-        if (response.ok) {
-            const newHistory = await response.json();
-            this.state.services = newHistory.services
-            console.log(newHistory)
-            const filtered = this.state.services.filter(obj =>{
-                return obj.vin === this.state.vin
-            })
-            console.log(filtered)
-            const cleared = {
-                service: ''
-            }
-            this.setState(cleared)
-            console.log(newState)
-        } 
+    // async handleOnSubmit(event) {
+    //     event.preventDefault()
+    //     const newState = [...this.state ]
+    //     delete newState.services 
+    //     // const servicesUrl = `http://localhost:8080/api/service/`;
+    //     // const fetchConfig = {
+    //     //     method: "get",
+    //     //     headers: { 'Content-Type': 'application/json' },
+    //     // }
+    //     // const response = await fetch(servicesUrl, fetchConfig)
+    //     // if (response.ok) {
+    //         // const newHistory = await response.json();
+    //         // this.state.services = newHistory.services
+    //         // console.log(newHistory)
+    //     const filtered = this.state.services.filter(obj =>{
+    //         return obj.vin === this.state.vin
+    //     })
+    //     console.log(filtered)
+    //     const cleared = {
+    //         service: ''
+    //     }
+    //     this.setState(cleared)
+    //     console.log(newState)
+    //     // } 
 
         
-    }
+    // }
     
 
     handleVinChange(event) {
@@ -68,11 +68,9 @@ import React from 'react';
     return (
         <>
         <h1>Service Appointment History</h1>
-        <form onSubmit={this.handleOnSubmit} id="search-vin" method='get'>
         <input value={this.state.vin} onChange={this.handleVinChange}
         type ='search' className='table table-hover' id='vin'
         placeholder='Enter VIN' />
-        </form>
         <table className="table table-striped">
         <thead>
           <tr>
@@ -86,7 +84,19 @@ import React from 'react';
           </tr>
         </thead>
         <tbody>
-        {this.state.services.filter(
+        {this.state.vin ==='' ? this.state.services.map(appointment => {
+            return (
+            <tr key= {appointment.id}>
+            <td>{appointment.vin}</td>
+            <td>{appointment.customer}</td>
+            <td>{appointment.date}</td>
+            <td>{appointment.time}</td>
+            <td>{appointment.description}</td>
+            <td>{appointment.technician.name}</td>
+            <td>{appointment.status.name}</td>
+            </tr>
+            );
+        }) : this.state.services.filter(
             service => service.vin === this.state.vin).map(
                 appointment => {
             return (
